8cf91fb3a02d80bf3a190e8ac05b29b910fa253d
Shreyas Shivaprakash <shreyas.shivaprakash@bestbuy.com>
2018-06-15 13:56:29 -0500

    Add POST /todos route and refactor mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,25 +7,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-// db.collection('Todos').insertOne({
-//   text: 'Something to do',
-//   completed: false
-// }, (err, result) => {
-//   if(err) {
-//     return console.log('unable to insert todo', err);
-//   }
-//   console.log(JSON.stringify(result.ops, undefined, 2));
-// });
+db.collection('Todos').insertOne({
+  text: 'Something to do',
+  completed: false
+}, (err, result) => {
+  if(err) {
+    return console.log('unable to insert todo', err);
+  }
+  console.log(JSON.stringify(result.ops, undefined, 2));
+});
 
-// db.collection('Users').insertOne({
-//   name: 'Shreyas',
-//   age: 29,
-//   location: 'Minneapolis'
-// }, (err, result) => {
-//   if(err) {
-//     return console.log('unable to insert user', err);
-//   }
-//   console.log(JSON.stringify(result.ops, undefined, 2));
-// });
+db.collection('Users').insertOne({
+  name: 'Shreyas',
+  age: 29,
+  location: 'Minneapolis'
+}, (err, result) => {
+  if(err) {
+    return console.log('unable to insert user', err);
+  }
+  console.log(JSON.stringify(result.ops, undefined, 2));
+});
    db.close();
 });
